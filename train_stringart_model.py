08bcaf051bb1ae452284d88821aca8c0b1a8b68f
Tywin Cary <tywincary@gmail.com>
2024-12-02 16:46:25 -0800

    adding training logs

--- train_stringart_model.py
+++ train_stringart_model.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 from stringart import StringArtGenerator
 import torch
 import config
+from evaluate import evaluate_model
 
 # Set up the generator
 generator = StringArtGenerator(
@@ -11,10 +13,21 @@ generator.set_nails(config.NAILS)
 generator.initialize_rl_model()
 
 # Train for multiple epochs
-epochs = 2
+epochs = 10
+
+mse_values = []
+
 for epoch in range(epochs):
-    print(f"Epoch {epoch + 1}/{epochs}")
     pattern = generator.generate()
+    mse, _ = evaluate_model(generator, pattern)
+    mse_values.append(mse)
+
+# Plot MSE over epochs
+plt.plot(mse_values)
+plt.title("Training Progress")
+plt.xlabel("Epoch")
+plt.ylabel("MSE")
+plt.show()
 
 # Save the model
 torch.save(generator.model.state_dict(), "string_art_model.pth")
